39d7e59f940f4c3a7965470ee4c55a559a4b2342
Yen Colon <yendejesusc41@gmail.com>
2020-12-06 17:39:12 -0400

    config doctrine orm

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
+            'driver' => 'doctrine',
             'model' => App\Models\User::class,
         ],
 
