fc41c0e08003c13d7c08aadfd4fe8547b9dbea18
Yen Colon <yendejesusc41@gmail.com>
2020-12-08 14:07:14 -0400

    adding seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Wallet;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use LaravelDoctrine\ORM\Facades\EntityManager;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +18,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $user = new User('Jhon', 'Smith', '12345678', '22920784', 'jhon@email.com', Hash::make('12345678'));
+        $wallet = new Wallet(0);
+        $user->setWallet($wallet);
+        EntityManager::persist($user);
+        EntityManager::persist($wallet);
+
+        EntityManager::flush();
     }
 }
