2019a13341572da991542a79d8f0bfb604ce323f
Yen Colon <yendejesusc41@gmail.com>
2020-12-10 10:12:32 -0400

    updating seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $user = new User('Jhon', 'Smith', '12345678', '22920784', 'jhon@email.com', Hash::make('12345678'));
         $wallet = new Wallet(0);
-        $user->setWallet($wallet);
+        $wallet->setUser($user);
         EntityManager::persist($user);
         EntityManager::persist($wallet);
 
